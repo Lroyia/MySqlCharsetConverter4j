7ae8d66920a56cb557d9218442413017aeec53f9
lroyia <814876716@qq.com>
2022-02-16 17:44:14 +0800

    init

---
+++ src/main/java/io/lroyia/converter/ApplicationRun.java
@@ -0,0 +1,13 @@
+package io.lroyia.converter;
+
+/**
+ * 程序执行
+ * @author lroyia
+ * @since 2022/2/16 17:34
+ **/
+public class ApplicationRun {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/io/lroyia/converter/util/SQLUtil.java
@@ -0,0 +1,22 @@
+package io.lroyia.converter.util;
+
+/**
+ * 数据库查询工具
+ * @author lroyia
+ * @since 2022/2/16 17:39
+ **/
+public class SQLUtil {
+
+    private static String HOST = "";
+
+    private static String USER_NAME = "";
+
+    private static String PASSWORD = "";
+
+    private static String DB_NAME = "";
+
+    public static void alterTable(String sql){
+
+    }
+
+}
